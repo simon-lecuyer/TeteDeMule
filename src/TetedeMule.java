8030405bf2affc0f9474e0048a8742c94aa3aa99
Simon Lécuyer <simlec2002@gmail.com>
2024-12-03 08:52:47 +0100

    chore-automate java project

--- src/TetedeMule.java
+++ src/TetedeMule.java
@@ -1,5 +1,5 @@
-public class TetedeMule {
-    public static void main(String[] args) {
-        System.out.println("Yo le sang");
+public class TeteDeMule {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
