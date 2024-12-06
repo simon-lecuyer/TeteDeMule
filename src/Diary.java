dc70e300f2e593442b2b12eb975ecb64c1a30cdd
GSablayrolles <g.sablayrolles@proton.me>
2024-12-06 13:13:47 +0100

    fix: Diary interface

--- src/Diary.java
+++ src/Diary.java
@@ -2,12 +2,10 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Diary extends Remote {
-
-    public void createFile(String fileName, String username) throws RemoteException;
-
-    public void updateFile(String fileName, String[] usernames) throws RemoteException;
-
-    public String[] getFileUsers(String fileName) throws RemoteException;
-
+    
+    public void addFileUsers(String fileName, String[] usernames) throws RemoteException;
+    
     public void deleteFileUsers(String fileName, String[] usernames) throws RemoteException;
+    
+    public String[] getFileUsers(String fileName) throws RemoteException;
 }
