ef10d24da080156aca69e1018b274bd551f796b1
Simon Lécuyer <simlec2002@gmail.com>
2024-12-13 09:20:03 +0100

    chore-update DiaryImplTest

--- src/DaemonImplTest.java
+++ src/DaemonImplTest.java
@@ -1,11 +1,9 @@
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
 import java.nio.file.*;
 import java.io.IOException;
 import java.rmi.RemoteException;
 import java.net.UnknownHostException;
 import java.net.InetAddress;
+import java.util.Arrays;
 
 public class DaemonImplTest {
 
@@ -13,47 +11,53 @@ public class DaemonImplTest {
     private DiaryImpl diary;
     private Path testFolder;
 
-    @Before
     public void setUp() throws RemoteException, IOException {
         daemon = new DaemonImpl();
         diary = new DiaryImpl();
         testFolder = Files.createTempDirectory("testFolder");
     }
 
-    @Test
     public void testUpload() throws RemoteException, IOException {
-        String fileName = "testFile.txt";
+        String fileName = "testFile1.txt";
         Path filePath = Files.createFile(testFolder.resolve(fileName));
         Files.write(filePath, "0123456789".getBytes());
 
         daemon.fileRegistry.put(fileName, filePath.toString());
 
         byte[] fragment = daemon.upload(fileName, 0, 10);
-        assertNotNull(fragment);
-        assertEquals(10, fragment.length);
+        if (fragment == null || fragment.length != 10) {
+            System.out.println("testUpload failed");
+        } else {
+            System.out.println("testUpload passed");
+        }
     }
 
-    @Test
     public void testUploadFileNotFound() throws RemoteException {
         byte[] fragment = daemon.upload("nonExistentFile.txt", 0, 10);
-        assertNull(fragment);
+        if (fragment != null) {
+            System.out.println("testUploadFileNotFound failed");
+        } else {
+            System.out.println("testUploadFileNotFound passed");
+        }
     }
 
-    @Test
     public void testNotifyDiaryIn() throws RemoteException, UnknownHostException, IOException {
-        String fileName = "testFile.txt";
+        String fileName = "testFile2.txt";
         Path filePath = Files.createFile(testFolder.resolve(fileName));
         Files.write(filePath, "0123456789".getBytes());
 
         daemon.fileRegistry.put(fileName, filePath.toString());
 
         daemon.notifyDiaryIn(diary);
-        assertTrue(diary.getFileUsers(fileName).contains(InetAddress.getLocalHost().getHostName()));
+        if (!Arrays.asList(diary.getFileUsers(fileName)).contains(InetAddress.getLocalHost().getHostName())) {
+            System.out.println("testNotifyDiaryIn failed");
+        } else {
+            System.out.println("testNotifyDiaryIn passed");
+        }
     }
 
-    @Test
     public void testNotifyDiaryOut() throws RemoteException, UnknownHostException, IOException {
-        String fileName = "testFile.txt";
+        String fileName = "testFile3.txt";
         Path filePath = Files.createFile(testFolder.resolve(fileName));
         Files.write(filePath, "0123456789".getBytes());
 
@@ -61,6 +65,19 @@ public class DaemonImplTest {
 
         daemon.notifyDiaryIn(diary);
         daemon.notifyDiaryOut(diary);
-        assertFalse(diary.getFileUsers(fileName).contains(InetAddress.getLocalHost().getHostName()));
+        if (Arrays.asList(diary.getFileUsers(fileName)).contains(InetAddress.getLocalHost().getHostName())) {
+            System.out.println("testNotifyDiaryOut failed");
+        } else {
+            System.out.println("testNotifyDiaryOut passed");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        DaemonImplTest test = new DaemonImplTest();
+        test.setUp();
+        test.testUpload();
+        test.testUploadFileNotFound();
+        test.testNotifyDiaryIn();
+        test.testNotifyDiaryOut();
     }
 }
