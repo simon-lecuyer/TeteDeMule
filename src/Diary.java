6fe1fcfe1b10cd652a10506847b6bbe65e7808c2
Simon Lécuyer <simlec2002@gmail.com>
2024-12-18 11:05:31 +0100

    fix: Diary now use users

--- src/Diary.java
+++ src/Diary.java
@@ -1,11 +1,12 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface Diary extends Remote {
 
-    public void addFileUsers(String fileName, String[] usernames) throws RemoteException;
+    public void addFileUsers(String fileName, ArrayList<User> users) throws RemoteException;
 
-    public void deleteFileUsers(String fileName, String[] usernames) throws RemoteException;
+    public void deleteFileUsers(String fileName, ArrayList<User> users) throws RemoteException;
 
-    public String[] getFileUsers(String fileName) throws RemoteException;
+    public ArrayList<User> getFileUsers(String fileName) throws RemoteException;
 }
