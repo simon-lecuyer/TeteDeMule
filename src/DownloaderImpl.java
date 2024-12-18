8b6b45c1fbebe3f5290e693cb902f81d84c51cab
GSablayrolles <g.sablayrolles@proton.me>
2024-12-18 11:36:20 +0100

    feat: download

--- src/DownloaderImpl.java
+++ src/DownloaderImpl.java
@@ -1,8 +1,7 @@
-import java.lang.reflect.Array;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -16,6 +15,7 @@ public class DownloaderImpl implements Downloader {
         try {
             daemon = new HashMap<>();
             diary = (Diary) Naming.lookup("//" + diaryHost + "/diary");
+            System.out.println("Diary found");
         } catch (Exception e) {
             System.out.println("Cannot connect to diary");
             e.printStackTrace();
@@ -23,13 +23,15 @@ public class DownloaderImpl implements Downloader {
     }
 
     @Override
-    public void download(String host, String fileName, int fragmentSize) {
+    public void download(String host, String fileName) {
         try {
             ArrayList<User> users = diary.getFileUsers(fileName);
             for (User user : users) {
                 Daemon d = (Daemon) Naming.lookup("//" + user.getUsername() + "/daemon");
                 daemon.put(user.getUsername(), d);
             }
+            
+
         } catch (RemoteException e) {
             System.out.println("No users available !");
         }
@@ -53,4 +55,65 @@ public class DownloaderImpl implements Downloader {
             e.printStackTrace();
         }
     }
+
+    // public void run() {
+    //     try {
+    //         int target = rand.nextInt(nbHosts);
+    //         int portTarget = ports[target];
+    //         String hostTarget = hosts[target];
+
+    //         System.out.println("Balancing to socket : " + portTarget);
+
+    //         // Les stream I/O du client vers le LB
+
+    //         InputStreamReader clientIn;
+    //         clientIn = new InputStreamReader(socketClient.getInputStream());
+    //         PrintStream clientOut;
+    //         clientOut = new PrintStream(socketClient.getOutputStream());
+    //         String rq = new LineNumberReader(clientIn).readLine();
+    //         System.out.println();
+            
+
+    //         // Créer la connection entre LB/server
+
+    //         Socket web = new Socket(hostTarget, portTarget);
+    //         System.out.println(web);
+    //         // Les stream I/O du server
+
+    //         InputStreamReader webOut;
+    //         webOut = new InputStreamReader(web.getInputStream());
+    //         DataOutputStream webIn;
+    //         webIn = new DataOutputStream(web.getOutputStream());
+            
+
+    //         // Lire la requête du client et l'envoyer au server
+
+    //         webIn.write(rq.getBytes());
+    //         webIn.writeBytes("\n");
+
+    //         // Renvoyer la réponse du server au client
+
+    //         System.out.println("Lecture server");
+    //         char[] buffer = new char[1024];
+    //         var byteRead = webOut.read(buffer, 0, 1024);
+    //         System.out.println("Lu : " + byteRead);
+
+    //         // Envoie au client
+    //         clientOut.println(buffer);
+    //         clientOut.flush();
+
+
+    //         // Fermer les sockets et les stream
+
+    //         clientIn.close();
+    //         clientOut.close();
+    //         webIn.close();
+    //         webOut.close();
+    //         socketClient.close();
+    //         web.close();
+    //         System.out.println("Tout est fermé ");
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 }
