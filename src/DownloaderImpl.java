9150ec5701e82baa874ac8e43fb96bebd14e4c62
GSablayrolles <g.sablayrolles@proton.me>
2024-12-18 11:46:39 +0100

    feat: new modif

--- src/DownloaderImpl.java
+++ src/DownloaderImpl.java
@@ -41,79 +41,14 @@ public class DownloaderImpl implements Downloader {
         }
     }
 
-
+    // Regarder TD1 pour avoir le téléchargement de fragments sur plusieurs machines
 
     public static void main(String[] args) {
         try {
-            ServerSocket ss = new ServerSocket(8080);
-            while (true) {
-                Thread t = new Slave(ss.accept());
-                t.start();
-                
-            }
+            
         } catch (Exception e) {
-            e.printStackTrace();
+            // TODO: handle exception
         }
     }
 
-    // public void run() {
-    //     try {
-    //         int target = rand.nextInt(nbHosts);
-    //         int portTarget = ports[target];
-    //         String hostTarget = hosts[target];
-
-    //         System.out.println("Balancing to socket : " + portTarget);
-
-    //         // Les stream I/O du client vers le LB
-
-    //         InputStreamReader clientIn;
-    //         clientIn = new InputStreamReader(socketClient.getInputStream());
-    //         PrintStream clientOut;
-    //         clientOut = new PrintStream(socketClient.getOutputStream());
-    //         String rq = new LineNumberReader(clientIn).readLine();
-    //         System.out.println();
-            
-
-    //         // Créer la connection entre LB/server
-
-    //         Socket web = new Socket(hostTarget, portTarget);
-    //         System.out.println(web);
-    //         // Les stream I/O du server
-
-    //         InputStreamReader webOut;
-    //         webOut = new InputStreamReader(web.getInputStream());
-    //         DataOutputStream webIn;
-    //         webIn = new DataOutputStream(web.getOutputStream());
-            
-
-    //         // Lire la requête du client et l'envoyer au server
-
-    //         webIn.write(rq.getBytes());
-    //         webIn.writeBytes("\n");
-
-    //         // Renvoyer la réponse du server au client
-
-    //         System.out.println("Lecture server");
-    //         char[] buffer = new char[1024];
-    //         var byteRead = webOut.read(buffer, 0, 1024);
-    //         System.out.println("Lu : " + byteRead);
-
-    //         // Envoie au client
-    //         clientOut.println(buffer);
-    //         clientOut.flush();
-
-
-    //         // Fermer les sockets et les stream
-
-    //         clientIn.close();
-    //         clientOut.close();
-    //         webIn.close();
-    //         webOut.close();
-    //         socketClient.close();
-    //         web.close();
-    //         System.out.println("Tout est fermé ");
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    // }
 }
